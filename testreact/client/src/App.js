484e23e67d0812c53660740cbb4c36970046cfac
Tinymrsb <mrsbrittanyshaw0@gmail.com>
2022-09-30 22:19:05 -0500

    updates

---
+++ testreact/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>No</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
