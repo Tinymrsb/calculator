275479eae3de9e45fbc8f1fc0099ee12e78820aa
Tinymrsb <mrsbrittanyshaw0@gmail.com>
2022-10-02 19:56:43 -0500

    Updated SEO

--- calculator/client/src/App.js
+++ calculator/client/src/App.js
@@ -4,13 +4,14 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import Footer from "./components/Footer";
 
 const btnValues = [
-["C", "+-", "%", "/"],
-[7, 8, 9, "x"],
-[4, 5, 6, "-"],
-[1, 2, 3, "+"],
-[0, ".", "="],
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
 ];
 
 function App() {
@@ -19,15 +20,12 @@ function App() {
       <Wrapper>
         <Screen />
         <ButtonBox>
-{btnValues.flat().map((btn, i) => (
-  <Button 
-  value={btn}
-  key={i}
-  />
-))}
-
+          {btnValues.flat().map((btn, i) => (
+            <Button value={btn} key={i} />
+          ))}
         </ButtonBox>
       </Wrapper>
+      <Footer />
     </div>
   );
 }
---
+++ calculator/client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        Open-source code by Brittany Shaw available on{" "}
+        <a href="https://github.com/Tinymrsb/react/tree/main/calculator/client">
+          GitHub
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
